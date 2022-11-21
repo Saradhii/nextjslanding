f15039ef01ca7a67dc21e8c73a35b0f5607076f6
Saradhi <99667252+Saradhii@users.noreply.github.com>
2022-11-21 13:05:49 +0530

    Update useScrollPosition.ts

--- hooks/useScrollPosition.ts
+++ hooks/useScrollPosition.ts
@@ -1,6 +1,5 @@
 import { useScrollPosition as originalUseScrollPosition } from '@n8tb1t/use-scroll-position';
 
-
 declare type ElementRef = React.MutableRefObject<HTMLElement | undefined>;
 
 type Axis = { x: number; y: number };
