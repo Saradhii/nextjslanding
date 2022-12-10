e15917d074d49211b28373c1881d26d105f1d5f8
Saradhi <99667252+Saradhii@users.noreply.github.com>
2022-12-10 11:47:44 +0530

    Update Accordion.tsx

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -8,7 +8,6 @@ interface AccordionProps {
   title: string;
   isOpen?: boolean;
 }
-  
 export default function Accordion({ title, isOpen, children }: PropsWithChildren<AccordionProps>) {
   const [hasCollapsed, setHasCollapsed] = useState(!isOpen);
   const isActive = !hasCollapsed;
