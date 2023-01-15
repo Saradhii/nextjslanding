c264550a475f4586ec67f6f71c205d4e49367a4f
Saradhi <99667252+Saradhii@users.noreply.github.com>
2023-01-15 18:09:00 +0530

    Update env.ts

--- env.ts
+++ env.ts
@@ -4,3 +4,4 @@ export const EnvVars = {
   URL: 'https://next-saas-starter-ashy.vercel.app/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
+
